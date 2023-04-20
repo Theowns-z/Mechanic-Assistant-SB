e9e4691649b92e1aec9fbb3d82af5f29407a4f9d
Luis Olivares <luissolivaresp@gmail.com>
2023-04-20 12:10:02 -0500

    ⚪️ First Commit

---
+++ src/main/java/com/Theowns/MechanicalAsisstant/MechanicalAsisstantApplication.java
@@ -0,0 +1,13 @@
+package com.Theowns.MechanicalAsisstant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicalAsisstantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicalAsisstantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Theowns/MechanicalAsisstant/MechanicalAsisstantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Theowns.MechanicalAsisstant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicalAsisstantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
