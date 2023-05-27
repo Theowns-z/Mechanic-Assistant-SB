d0f346857272a2fdf6cc879d85924922cf04b7ea
Luis Olivares <luissolivaresp@gmail.com>
2023-05-27 00:04:19 -0500

    🔵 Update Conductor controller change the controller type

--- src/main/java/com/Theowns/controllers/ConductorController.java
+++ src/main/java/com/Theowns/controllers/ConductorController.java
@@ -25,42 +25,40 @@ public class ConductorController {
 
 
     @GetMapping("/{id}")
-    public ResponseObject<?> getOne(@PathVariable("id") Long id) throws ExceptionObjectNotFound{
+    public ResponseEntity<ResponseObject<?>> getOne(@PathVariable("id") Long id) throws ExceptionObjectNotFound{
         try{
-            return new ResponseObject<ConductorDAO>("Conductor con id " + id + " encontrado", conductorService.getOne(id));
+            return ResponseEntity.ok(new ResponseObject<ConductorDAO>("Conductor con id " + id + " encontrado", conductorService.getOne(id)));
         }catch (ExceptionObjectNotFound e){
-            return new ResponseObject<String>("Error! No encontrado", e.getMessage());
+            return ResponseEntity.status(404).body(new ResponseObject<String>("Error! No encontrado", e.getMessage()));
         }
 
     }
 
 
     @PostMapping()
-    public ResponseObject<?> save(@RequestBody ConductorModel conductor) throws DuplicateException {
+    public ResponseEntity<ResponseObject<?>> save(@RequestBody ConductorModel conductor) throws DuplicateException {
         try {
-        return new ResponseObject<ConductorModel>( "Conductor guardado correctamente",conductorService.save(conductor));
+        return ResponseEntity.ok( new ResponseObject<ConductorModel>( "Conductor guardado correctamente",conductorService.save(conductor)));
         }catch (DuplicateException e){
-            return new ResponseObject<String>("Error! No guardado",e.getMessage());
+            return ResponseEntity.status(404).body(new ResponseObject<String>("Error! No guardado",e.getMessage()));
         }
     }
 
     @PutMapping("/{id}")
-    public ResponseObject<?> update(@RequestBody ConductorModel conductor, @PathVariable("id") Long id) throws ExceptionObjectNotFound {
+    public ResponseEntity<ResponseObject<?>> update(@RequestBody ConductorModel conductor, @PathVariable("id") Long id) throws ExceptionObjectNotFound {
         try{
-            ConductorDAO   conductorDAO = conductorService.update(id,conductor);
-            return new ResponseObject<ConductorDAO>("Conductor actualizado correctamente", conductorDAO);
+            return ResponseEntity.ok(new ResponseObject<ConductorDAO>("Conductor actualizado correctamente", conductorService.update(id,conductor)));
         }catch (ExceptionObjectNotFound e){
-            return new ResponseObject<String>( "Error! No actualizado", e.getMessage());
+            return ResponseEntity.status(404).body(new ResponseObject<String>( "Error! No actualizado", e.getMessage()));
         }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseObject<?> delete(@PathVariable("id") Long id) throws ExceptionObjectNotFound{
+    public ResponseEntity<ResponseObject<?>> delete(@PathVariable("id") Long id) throws ExceptionObjectNotFound{
         try{
-            ResponseEntity.ok(conductorService.delete(id));
-            return new ResponseObject<String>("Conductor eliminado correctamente","Conductor con id " + id + " eliminado");
+            return ResponseEntity.ok(new ResponseObject<String>("Conductor eliminado correctamente","Conductor con id " + id + " eliminado"));
         }catch (ExceptionObjectNotFound e){
-            return new ResponseObject<String>("Error! No eliminado",e.getMessage());
+            return ResponseEntity.status(404).body(new ResponseObject<String>("Error! No eliminado",e.getMessage()));
         }
     }
 }
--- src/main/java/com/Theowns/controllers/UserController.java
+++ src/main/java/com/Theowns/controllers/UserController.java
@@ -1,23 +1,47 @@
 package com.Theowns.controllers;
 
+import com.Theowns.DAO.AuthenticationRequest;
+import com.Theowns.config.TokenUtils;
 import com.Theowns.models.ConductorModel;
 import com.Theowns.models.UserModel;
+import com.Theowns.repositories.UserRepository;
 import com.Theowns.services.UserService;
 import com.Theowns.services.exceptions.DuplicateException;
+import com.Theowns.services.exceptions.ExceptionObjectNotFound;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class UserController {
+    private final AuthenticationManager authenticationManager;
     @Autowired
-    UserService userService;
-    @PostMapping()
-    public ResponseEntity<UserModel> save(@RequestBody UserModel user) throws DuplicateException {
-        return ResponseEntity.ok( userService.save(user));
+    private UserRepository userRepository;
+
+    @PostMapping("/login")
+public ResponseEntity<ResponseObject<?>> authenticate( @RequestBody AuthenticationRequest request) throws ExceptionObjectNotFound {
+        authenticationManager
+            .authenticate( new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+
+        UserModel user = userRepository.findByEmail(request.getEmail())
+            .orElseThrow(()->new ExceptionObjectNotFound("Usuario no encontrado"));
+
+        if(user != null){
+            return ResponseEntity.ok(new ResponseObject<String>(
+                    "Se ha loggeado correctamente",
+                    TokenUtils.createToken(user.getRole().name(),user.getEmail())));
+        }
+
+        return ResponseEntity.status(400).body(new ResponseObject<String>("Error! No loggeado","Ha ocurrido un error, comprueba tus credenciales"));
     }
 }
